929dbc6105655e30cace3a01687904ca20f1bc53
Volodymyr Chernyavskyi <vovikche@gmail.com>
2023-09-22 15:57:53 +0300

    feat (scripts): created script of Zenject installer for scene default gameobjects

---
+++ Assets/Scripts/Zenject Installers/SceneLogicInstaller.cs
@@ -0,0 +1,18 @@
+using SpaceShooter.GameLogic;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace SpaceShooter.Installers
+{
+    public class SceneLogicInstaller : MonoInstaller
+    {
+        public ScreenFader ScreenFader;
+
+        public override void InstallBindings()
+        {
+            Container.BindInstance(ScreenFader).AsSingle().NonLazy();
+        }
+    }
+}
