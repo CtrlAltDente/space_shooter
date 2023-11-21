f7779f51fd5bff06f2018f8eb0895eb4bd5706c5
Volodymyr Chernyavskyi <vovikche@gmail.com>
2023-11-21 10:26:47 +0200

    fix (scripts): fixed many error moments

---
+++ Assets/Scripts/Network/Session/SessionsLocator.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TMPro;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SpaceShooter.Network
+{
+    public class SessionsLocator : MonoBehaviour
+    {
+        [SerializeField]
+        private SessionsList _sessionsList;
+
+        private UdpClient _udpClient;
+
+        private Coroutine _networkCoroutine;
+
+        public void StartSessionSearch()
+        {
+            StartUdpClient(LocalNetworkInfo.DefaultPort);
+            _networkCoroutine = StartCoroutine(StartSessionListening());
+        }
+
+        public void CloseUdpClient()
+        {
+            if (_udpClient != null)
+            {
+                StopCoroutine(_networkCoroutine);
+                _udpClient.Close();
+                _udpClient = null;
+                Debug.Log("UDP CLIENT DESTROYED");
+            }
+        }
+
+        private void StartUdpClient(int port)
+        {
+            _udpClient = new UdpClient(port);
+            _udpClient.JoinMulticastGroup(LocalNetworkInfo.DefaultMulticastIpAddress);
+        }
+
+        private IEnumerator StartSessionListening()
+        {
+            while (_udpClient != null)
+            {
+                yield return new WaitForSeconds(1f);
+                ReceiveSession();
+            }
+        }
+
+        private async void ReceiveSession()
+        {
+            if (_udpClient != null)
+            {
+                var receiveResult = await _udpClient.ReceiveAsync();
+                Debug.Log("Received data");
+                _sessionsList.ProceedReceivedBytes(receiveResult.Buffer);
+            }
+        }
+    }
+}
--- Assets/Scripts/Network/Session/SessionsSharer.cs
+++ Assets/Scripts/Network/Session/SessionsSharer.cs
@@ -1,25 +1,16 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using TMPro;
-using Unity.Netcode;
-using Unity.Netcode.Transports.UTP;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace SpaceShooter.Network
 {
-    public class SessionsManager : MonoBehaviour
+    public class SessionsSharer : MonoBehaviour
     {
         private UdpClient _udpClient;
-
-        [SerializeField]
-        private SessionsList _sessionsList;
 
         [SerializeField]
         private TextMeshProUGUI _sessionId;
@@ -29,41 +20,29 @@ namespace SpaceShooter.Network
         [SerializeField]
         private SessionInfo _createdSessionInfo;
 
-        private void OnDestroy()
-        {
-            CloseUdpClient();
-        }
+        private Coroutine _networkCoroutine;
 
         public void StartSession()
         {
             string localIpAddress = LocalNetworkInfo.GetLocalIPAddress();
-            _createdSessionInfo = new SessionInfo(UnityEngine.Random.Range(10001, 19999), localIpAddress);
+            _createdSessionInfo = new SessionInfo(Random.Range(10001, 19999), localIpAddress);
 
             _sessionId.text = $"Session Id: {_createdSessionInfo.SessionId}";
             _sessionIp.text = $"Session Ip: {_createdSessionInfo.SessionIp}";
 
             StartUdpClient(LocalNetworkInfo.DefaultPort + 1);
-            StartCoroutine(StartSessionSharing());
-        }
-
-        public void StartSessionSearch()
-        {
-            StartUdpClient(LocalNetworkInfo.DefaultPort);
-            StartCoroutine(StartSessionListening());
+            _networkCoroutine = StartCoroutine(StartSessionSharing());
         }
 
         public void CloseUdpClient()
         {
             if (_udpClient != null)
             {
-                SendSession(new SessionInfo(_createdSessionInfo.SessionId, "0.0.0.0"));
-                _udpClient.DropMulticastGroup(LocalNetworkInfo.DefaultMulticastIpAddress);
-                _udpClient.Close();
-                _udpClient = null;
+                StopCoroutine(_networkCoroutine);
+                SendSession(new SessionInfo(_createdSessionInfo.SessionId, "0.0.0.0"), true);
                 Debug.Log("UDP CLIENT DESTROYED");
             }
         }
-
 
         private void StartUdpClient(int port)
         {
@@ -81,31 +60,18 @@ namespace SpaceShooter.Network
             }
         }
 
-        private IEnumerator StartSessionListening()
-        {
-            while (_udpClient != null)
-            {
-                yield return new WaitForSeconds(1f);
-                ReceiveSession();
-            }
-        }
-
-        private async void SendSession(SessionInfo sessionInfo)
+        private async void SendSession(SessionInfo sessionInfo, bool destroyAtEnd = false)
         {
             if (_udpClient != null)
             {
                 byte[] sessionInfoData = Encoding.UTF8.GetBytes(JsonUtility.ToJson(sessionInfo));
                 await _udpClient.SendAsync(sessionInfoData, sessionInfoData.Length, new IPEndPoint(LocalNetworkInfo.DefaultMulticastIpAddress, LocalNetworkInfo.DefaultPort));
-            }
-        }
 
-        private async void ReceiveSession()
-        {
-            if (_udpClient != null)
-            {
-                var receiveResult = await _udpClient.ReceiveAsync();
-                Debug.Log("Received data");
-                _sessionsList.ProceedReceivedBytes(receiveResult.Buffer);
+                if (destroyAtEnd)
+                {
+                    _udpClient.Close();
+                    _udpClient = null;
+                }
             }
         }
     }
