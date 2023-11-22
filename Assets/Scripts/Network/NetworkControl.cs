56ae9fca280cdce403120d70f1c512d0ecea9f58
CtrlAltDente <vovikche@gmail.com>
2023-11-22 21:51:26 +0200

    feat (networking): configured testable version of relay connection

--- Assets/Scripts/Network/NetworkControl.cs
+++ Assets/Scripts/Network/NetworkControl.cs
@@ -3,9 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay.Models;
+using Unity.Services.Relay;
 using UnityEngine;
 using UnityEngine.Events;
 using Zenject;
+using System.Threading.Tasks;
 
 namespace SpaceShooter.Network
 {
@@ -14,9 +20,12 @@ namespace SpaceShooter.Network
         public UnityEvent OnHostSelected;
         public UnityEvent OnClientSelected;
 
+        public UnityEvent<string> OnRelayHostSelected;
+        public UnityEvent OnRelayClientSelected;
+
         [SerializeField]
         private SceneLoader _sceneLoader;
-        
+
         private void Start()
         {
             SubscribeOnEvents();
@@ -25,6 +34,35 @@ namespace SpaceShooter.Network
         private void OnDestroy()
         {
             UnsubscribeFromEvents();
+        }
+
+        public async void SelectRelayHost()
+        {
+            Task<string> task = StartHostWithRelay();
+            await task;
+            if (task.Result != null)
+            {
+                Debug.Log($"Join code: {task.Result}");
+                OnRelayHostSelected?.Invoke(task.Result);
+            }
+            else
+            {
+                Debug.Log("Failed to start host on relay!");
+            }
+        }
+
+        public async void SelectRelayClient(string joinCode)
+        {
+            Task<bool> task = StartClientWithRelay(joinCode);
+            await task;
+            if (task.Result)
+            {
+                OnRelayClientSelected?.Invoke();
+            }
+            else
+            {
+                Debug.Log("Cannot start client on this join code!");
+            }
         }
 
         public void SelectHost()
@@ -59,6 +97,32 @@ namespace SpaceShooter.Network
             {
                 NetworkManager.Singleton.OnClientStopped -= Shutdown;
             }
+        }
+
+        public async Task<string> StartHostWithRelay(int maxConnections = 5)
+        {
+            await UnityServices.InitializeAsync();
+            if (!AuthenticationService.Instance.IsSignedIn)
+            {
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            }
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, "dtls"));
+            var joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            return NetworkManager.Singleton.StartHost() ? joinCode : null;
+        }
+
+        public async Task<bool> StartClientWithRelay(string joinCode)
+        {
+            await UnityServices.InitializeAsync();
+            if (!AuthenticationService.Instance.IsSignedIn)
+            {
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            }
+
+            var joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode: joinCode);
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, "dtls"));
+            return !string.IsNullOrEmpty(joinCode) && NetworkManager.Singleton.StartClient();
         }
 
         private void ShutdownNetwork()
