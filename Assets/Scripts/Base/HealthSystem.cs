a0359704c3070a0bbc0ce529b898d203a9aa8d03
Volodymyr Chernyavskyi <vovikche@gmail.com>
2023-12-22 15:54:20 +0200

    fix (scripts); small fix

--- Assets/Scripts/Base/HealthSystem.cs
+++ Assets/Scripts/Base/HealthSystem.cs
@@ -56,6 +56,10 @@ namespace SpaceShooter.Base
                 {
                     EnergyShield += _restorationSpeed * Time.deltaTime;
                 }
+                if (EnergyShield >= BaseEnergyShield)
+                {
+                    EnergyShield = BaseEnergyShield;
+                }
 
                 yield return null;
             }
