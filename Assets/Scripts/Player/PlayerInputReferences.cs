5b36d12a4f7f16221aeb496ea651291eee14d930
Volodymyr Chernyavskyi <vovikche@gmail.com>
2024-01-03 13:41:05 +0200

    reafactoring: added namespace to PlayerInputReferences

--- Assets/Scripts/Player/PlayerInputReferences.cs
+++ Assets/Scripts/Player/PlayerInputReferences.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerInputReferences : MonoBehaviour
+namespace SpaceShooter.Player
 {
-    public bool LeftTriggerPressed => LeftTriggerActionReference.action.IsPressed();
-    public bool RightTriggerPressed => RightTriggerActionReference.action.IsPressed();
+    public class PlayerInputReferences : MonoBehaviour
+    {
+        public bool LeftTriggerPressed => LeftTriggerActionReference.action.IsPressed();
+        public bool RightTriggerPressed => RightTriggerActionReference.action.IsPressed();
 
-    public bool LeftStickPressed => LeftStickActionReference.action.triggered;
-    public bool RightStickPressed => RightStickActionReference.action.triggered;
+        public bool LeftStickPressed => LeftStickActionReference.action.triggered;
+        public bool RightStickPressed => RightStickActionReference.action.triggered;
 
-    [SerializeField]
-    private InputActionReference LeftTriggerActionReference;
-    [SerializeField]
-    private InputActionReference RightTriggerActionReference;
+        [SerializeField]
+        private InputActionReference LeftTriggerActionReference;
+        [SerializeField]
+        private InputActionReference RightTriggerActionReference;
 
-    [SerializeField]
-    private InputActionReference LeftStickActionReference;
-    [SerializeField]
-    private InputActionReference RightStickActionReference;
+        [SerializeField]
+        private InputActionReference LeftStickActionReference;
+        [SerializeField]
+        private InputActionReference RightStickActionReference;
+    }
 }
