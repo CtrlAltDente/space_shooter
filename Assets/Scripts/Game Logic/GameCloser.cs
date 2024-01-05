f9bf4a54433bf1d68c2d6eb991de4c1cf21deed8
Volodymyr Chernyavskyi <vovikche@gmail.com>
2024-01-05 10:12:17 +0200

    feat (scripts): moved script to another folder

--- Assets/Scripts/Game Logic/GameCloser.cs
+++ Assets/Scripts/Game Logic/GameCloser.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-namespace SpaceShooter.Base
+namespace SpaceShooter.GameLogic
 {
     public class GameCloser : MonoBehaviour
     {
