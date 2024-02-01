59120ef5d10714612ea0ddbc1fadb85e34c4ec9f
Volodymyr Chernyavskyi <vovikche@gmail.com>
2024-02-01 10:48:25 +0200

    feat (game data): refactored code for easier getting and setting values, implement new game datas

--- Assets/Scripts/Data/GameData.cs
+++ Assets/Scripts/Data/GameData.cs
@@ -7,23 +7,41 @@ namespace SpaceShooter.GameLogic
 {
     public static class GameData
     {
+        #region Values 
+
         public static int PlayerConfigurationIndex
         {
             get
             {
-                if (PlayerPrefs.HasKey(GameDataKeys.PLAYER_CONFIGURATION_INDEX))
-                {
-                    return PlayerPrefs.GetInt(GameDataKeys.PLAYER_CONFIGURATION_INDEX);
-                }
-                else
-                {
-                    PlayerPrefs.SetInt(GameDataKeys.PLAYER_CONFIGURATION_INDEX, 0);
-                    return 0;
-                }
+                return GetStringValue(GameDataKeys.PLAYER_CONFIGURATION_INDEX);
             }
             set
             {
-                PlayerPrefs.SetInt(GameDataKeys.PLAYER_CONFIGURATION_INDEX, value);
+                SetValue(GameDataKeys.PLAYER_CONFIGURATION_INDEX, value);
+            }
+        }
+
+        public static int SkinIndex
+        {
+            get
+            {
+                return GetIntValue(GameDataKeys.SKIN_INDEX);
+            }
+            set
+            {
+                SetValue(GameDataKeys.SKIN_INDEX, value);
+            }
+        }
+
+        public static int GunIndex
+        {
+            get
+            {
+                return GetIntValue(GameDataKeys.GUN_INDEX);
+            }
+            set
+            {
+                SetValue(GameDataKeys.GUN_INDEX, value);
             }
         }
 
@@ -31,27 +49,61 @@ namespace SpaceShooter.GameLogic
         {
             get
             {
-                if (PlayerPrefs.HasKey(GameDataKeys.PLAYER_NAME))
-                {
-                    return PlayerPrefs.GetString(GameDataKeys.PLAYER_NAME);
-                }
-                else
-                {
-                    PlayerPrefs.SetString(GameDataKeys.PLAYER_NAME, "Player");
-                    return PlayerPrefs.GetString(GameDataKeys.PLAYER_NAME);
-                }
+                return GetStringValue(GameDataKeys.PLAYER_NAME);
             }
             set
             {
-                if (value != null)
-                {
-                    PlayerPrefs.SetString(GameDataKeys.PLAYER_NAME, value);
-                }
-                else
-                {
-                    PlayerPrefs.SetString(GameDataKeys.PLAYER_NAME, string.Empty);
-                }
+                SetValue(GameDataKeys.PLAYER_NAME, value);
             }
         }
+
+        #endregion
+
+        #region Getting and setting values to PlayerPrefs logic
+
+        private static int GetIntValue(string key)
+        {
+            if (PlayerPrefs.HasKey(key))
+            {
+                return PlayerPrefs.GetInt(key);
+            }
+            else
+            {
+                PlayerPrefs.SetInt(key, 0);
+                return 0;
+            }
+        }
+
+        private static string GetStringValue(string key)
+        {
+            if (PlayerPrefs.HasKey(key))
+            {
+                return PlayerPrefs.GetString(key);
+            }
+            else
+            {
+                SetValue(key, "text");
+                return PlayerPrefs.GetString(key);
+            }
+        }
+
+        private static void SetValue(string key, int value)
+        {
+            PlayerPrefs.SetInt(key, value);
+        }
+
+        private static void SetValue(string key, string value)
+        {
+            if (value != null)
+            {
+                PlayerPrefs.SetString(key, value);
+            }
+            else
+            {
+                PlayerPrefs.SetString(key, string.Empty);
+            }
+        }
+
+        #endregion
     }
 }
