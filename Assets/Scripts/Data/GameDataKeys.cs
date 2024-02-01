e0aae27d54df9508c58e33cc83d50fe3e3b1bcc1
Volodymyr Chernyavskyi <vovikche@gmail.com>
2024-02-01 10:40:01 +0200

    feat (game data): added new variables of game data keys

--- Assets/Scripts/Data/GameDataKeys.cs
+++ Assets/Scripts/Data/GameDataKeys.cs
@@ -7,6 +7,8 @@ namespace SpaceShooter.GameLogic
     public static class GameDataKeys
     {
         public static readonly string PLAYER_CONFIGURATION_INDEX = "PlayerConfigurationIndex";
+        public static readonly string SKIN_INDEX = "SkinIndex";
+        public static readonly string GUN_INDEX = "GunIndex";
         public static readonly string PLAYER_NAME = "PlayerName";
     }
 }
