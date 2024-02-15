c23d9a1e5365b67e1d8bf3434a4b682d3118320f
Volodymyr Chernyavskyi <vovikche@gmail.com>
2024-02-15 10:15:11 +0200

    feat (scripts): created container of session configuration

---
+++ Assets/Scripts/Enums/SessionType.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooter.Enums
+{
+    public enum SessionType : int
+    {
+        Time,
+        EnemyKills
+    }
+}
