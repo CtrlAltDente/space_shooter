9939308d3a732d4f76222e4dd6ada4f92b44a576
Volodymyr Chernyavskyi <vovikche@gmail.com>
2024-02-19 16:05:53 +0200

    feat (scripts): configured display of life support system information

--- Assets/Scripts/User/Configuration/Displayers/LifeSupportSystemDisplayer.cs
+++ Assets/Scripts/User/Configuration/Displayers/LifeSupportSystemDisplayer.cs
@@ -1,6 +1,7 @@
 using SpaceShooter.LifeSupport;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace SpaceShooter.User.Configuration
@@ -10,9 +11,20 @@ namespace SpaceShooter.User.Configuration
         [SerializeField]
         private LifeSupportSystem _lifeSupportSystem;
 
+        [SerializeField]
+        private TextMeshProUGUI _informationText;
+
         public override void DisplayCurrentConfig(int configDataIndex)
         {
             _lifeSupportSystem.InitilizeMesh(_configChooser.Container.Items[configDataIndex]);
+            ShowInformation(_configChooser.Container.Items[configDataIndex]);
+        }
+
+        private void ShowInformation(LifeSupportSystem lifeSupportSystem)
+        {
+            _informationText.text = $"Health: {lifeSupportSystem.Health.Health}\n" +
+                $"Energy shield: {lifeSupportSystem.Health.EnergyShield}\n" +
+                $"Shield restoration speed: {lifeSupportSystem.Health.EnergyRestorationSpeed}/second";
         }
     }
 }
